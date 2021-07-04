9854fc42b724acf301a432a61d5a11c531e06da9
yiming <ymtan.2062@gmail.com>
2021-07-04 22:37:57 +0100

    reformatted code

--- routes/downloadCV.js
+++ routes/downloadCV.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     const file = `${__dirname}/../resources/YiMingTan.pdf`;
     res.download(file); // Set disposition and send it.
 });
